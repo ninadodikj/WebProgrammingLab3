3f3da9ca56edbedf95c0a2a9608516be9db07ae9
ninadodikj <dodicnina57@gmail.com>
2025-12-10 23:54:11 +0100

    Lab3

--- src/main/java/mk/ukim/finki/wp/lab/repository/ChefRepository.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/ChefRepository.java
@@ -1,13 +1,8 @@
 package mk.ukim.finki.wp.lab.repository;
 
 import mk.ukim.finki.wp.lab.model.Chef;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface ChefRepository {
-    List<Chef> findAll();
-    Optional<Chef> findById(Long id);
-    Chef save(Chef chef);
+public interface ChefRepository extends JpaRepository<Chef,Long> {
 
 }
